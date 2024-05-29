e0f9cb266d7c59ea5bfd3008f6f00588543efb89
Adrian <marin.mag@stx.ox.ac.uk>
2024-05-29 15:11:09 +0100

    d

--- examples/test.py
+++ examples/test.py
@@ -1,10 +1,14 @@
-from axikernels.core.handlers import station_output
-import matplotlib
-matplotlib.use('tkagg')
-import matplotlib.pyplot as plt
+import numpy as np
 
-my_output = station_output.StationOutput('data/EXAMPLE/output/stations/Station_grid')
-data = my_output.stream(networks=['II'], station_names=['CMLA'], locations=[''], channels=['UZ'])
-print(data)
-data.plot()
-plt.show()
+# Create a 2D numpy array
+arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
+
+# Create index arrays that contain all indices
+row_indices = np.array([0, 1, 2])
+col_indices = np.array([0, 1, 2])
+
+# Use the index arrays to select all rows and columns from the original array
+selected_rows = arr[row_indices]
+selected_cols = arr[:, col_indices]
+# This will output the diagonal elements of arr
+print(arr[row_indices][:, col_indices])
