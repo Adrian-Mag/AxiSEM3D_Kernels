16692416d1d670f812ccc328f47ec1fc334767e3
Adrian Mag <zcfbagx@ucl.ac.uk>
2024-06-01 20:36:00 +0100

    a

--- examples/data/EXAMPLE_FORWARD/test.py
+++ examples/data/EXAMPLE_FORWARD/test.py
@@ -1,11 +1,11 @@
 import yaml
 
 # Load the YAML file
-with open('../adjoint_source_model.yaml', 'r') as file:
+with open('input/inparam.source.yaml', 'r') as file:
     data = yaml.safe_load(file)
 
 # Modify the record_length value
-data['time_axis']['record_length'] = 7200.  # Change this to your desired value
+data['time_axis']['record_length'] = 360.  # Change this to your desired value
 
 
 # Save the modified YAML file
