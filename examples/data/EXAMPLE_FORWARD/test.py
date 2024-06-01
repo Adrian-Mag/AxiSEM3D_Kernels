736c60f8a250bc13cb857f44b91c8840b2b06341
Adrian Mag <zcfbagx@ucl.ac.uk>
2024-06-01 21:02:58 +0100

    aa

--- examples/data/EXAMPLE_FORWARD/test.py
+++ examples/data/EXAMPLE_FORWARD/test.py
@@ -1,13 +1,15 @@
-import yaml
+from ruamel.yaml import YAML
+
+# Create a YAML object
+yaml = YAML()
 
 # Load the YAML file
 with open('input/inparam.source.yaml', 'r') as file:
-    data = yaml.safe_load(file)
+    data = yaml.load(file)
 
 # Modify the record_length value
-data['time_axis']['record_length'] = 360.  # Change this to your desired value
-
+data['time_axis']['record_length'] = 100.  # Change this to your desired value
 
 # Save the modified YAML file
 with open('inparam.source.yaml', 'w') as file:
-    yaml.safe_dump(data, file, default_flow_style=False)
+    yaml.dump(data, file)
