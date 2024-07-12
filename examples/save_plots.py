c9d8cab844e8912203eeb3a632492bd4b99a59d0
Adrian Mag <zcfbagx@ucl.ac.uk>
2024-07-12 09:06:02 +0100

    pushed kernels branch to main

--- examples/save_plots.py
+++
@@ -1,8 +0,0 @@
-from axikernels.aux.mesher import SliceMesh, SphereMesh
-import matplotlib.pyplot as plt
-
-""" new_mesh = SliceMesh('vp_3D_30s_P.h5')
-new_mesh.plot_on_mesh(cbar_range=[-5e-20, 5e-20], filename='vp_1D_30s_P.png') """
-
-new_mesh = SphereMesh('shell_Kd_1D_30s_P.h5')
-new_mesh.plot_on_mesh(remove_outliers=True, cbar_range=[-5e-17, 5e-17])
